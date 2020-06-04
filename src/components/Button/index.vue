3331f1a0ce951d2c746405a63152557ea0af3a2f
Eugenia <eugemiran@gmail.com>
2020-06-04 18:18:49 -0300

    Create button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    :class="{
+      'btn-primary': type === 'primary',
+      'btn-secondary': type === 'secondary'
+    }"
+    type="button"
+    @click="onClick"
+    :disabled="disabled"
+  >
+    <slot>Submit</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: Function,
+    disabled: Boolean,
+    type: String
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/constants";
+
+.btn-secondary {
+  background: $white;
+
+  &:hover:enabled {
+    background: rgba($tangerine, 0.7);
+    border: 1px solid $white;
+    color: $white;
+  }
+}
+
+.btn-primary {
+  background: $white;
+
+  &:hover:enabled {
+    background: rgba($atoll, 0.2);
+  }
+}
+</style>
